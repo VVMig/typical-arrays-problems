fd7dbd6fb22ca564d0f7cac483fd3d8aa0fe35a0
Vova <vova.migay123@gmail.com>
2020-02-21 20:49:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+  if(array===undefined) {
+    return 0;
+  }
+  return array.length > 0
+      ? array.reduce((min, e) => (e < min ? e : min), array[0])
+      : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if(array===undefined) {
+    return 0;
+  }
+  return array.length > 0
+      ? array.reduce((max, e) => (e > max ? e : max), array[0])
+      : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if(array===undefined) {
+    return 0;
+  }
+  return array.length > 0
+      ? array.reduce((avg, e) => avg + e, 0) / array.length
+      : 0;
+};
